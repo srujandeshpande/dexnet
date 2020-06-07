0178bf9eec6af339319742888cb78b4bc33f7e1f
Srujan Deshpande <srujan.deshpande@gmail.com>
2020-06-07 23:04:34 +0530

    Firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
+import {BrowserRouter,Switch,Route} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Homepage from './components/Homepage';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+import Profile from './components/Profile';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <Switch>
+        <Route path='/' component = {Homepage}/>
+
+      </Switch>
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Homepage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Homepage extends React.Component {
+    render(){
+    return (
+        <div>
+          <br/>
+          <h1>This is Inhuman Cards.</h1>
+          <h6>How to play? You should know. You're the one who came here.</h6>
+        </div>
+    )
+    }
+
+}
+export default Homepage
